2cbcc920309beb92d22c29a66070fd89096eb3f2
Colin Jaffe <balloonasaurus@gmail.com>
2022-12-13 09:21:23 -0500

    Revert "Initial commit."
    This reverts commit 50775b722ab4cebb2b2db72d6e0bb2f266ad6977.

--- main.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('header li').remove()
-document.querySelector('.featured span').remove()
-document.querySelector('.posts article:last-child').remove()
-document.querySelectorAll('article h3').forEach((h3) => h3.remove())
